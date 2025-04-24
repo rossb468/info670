2081f00680f0f1dfd2017b51c61f04e8cd7b193b
Ross Bower <ross.bower@mlb.com>
2025-04-24 17:35:20 -0400

    File cleanup and adding readme

---
+++ todoList/README.md
@@ -0,0 +1,25 @@
+# Ross Bower's TODO List README
+
+Welcome! My TODO list is awsome and has many great features, including:
+
+* Adding items
+* Completing items
+* Deleting items
+
+By default the app launches with three example items.
+![Home page](homePage.png)
+
+## To add an item
+
+Type the text for the item into the "Todo..." box at the bottom of the screen, then tap "Add item".
+![Adding an item](addItem.png)
+
+## To complete an item
+
+Simply tap an item in the list to mark it as completed.
+![Completing an item](completeItem.png)
+
+## To delete an item
+
+To delete an item from the list (completed or uncompleted) long-press the item. A confirmation dialog will appear. Tap "Delete" to delete the item.
+![Deleting an item](deleteItem.png)
---
+++ todoList/todoList/App.js
@@ -0,0 +1,156 @@
+
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Button, Alert, Pressable, TextInput } from 'react-native';
+import { useState } from 'react';
+import uuid from 'react-native-uuid';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const MyList = props => {
+  
+  const [data, setList] = useState(DATA)
+  const [inputText,setInputText] = useState('')
+
+  function handleSubmit(e) {
+    const newList = data.concat({
+      id: uuid.v4(),
+      title: inputText
+    });
+
+    setList(newList)
+  }
+
+  function showConfirmAlert(id) {
+    Alert.alert(
+      'Delete Item',
+      'Are you sure you want to delete this item?',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'Delete',
+          onPress: () => setList((prevItems) => prevItems.filter((item) => item.id !== id)),
+          style: 'destructive',
+        },
+      ],
+      { cancelable: true }
+    );
+  };
+
+  const Item = ({title, id}) => {
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+    return (
+    <View style={styles.container}>
+      <Pressable 
+        onPress={() => toggleSwitch()}
+        onLongPress={() => showConfirmAlert(id)}
+        style={({ pressed }) => [
+          styles.rowItem,
+          { backgroundColor: pressed ? '#FFF' : 'white' }
+        ]}
+      >
+        <Text style={[
+          styles.title,
+          { color: isEnabled ? '#888' : 'black',
+            textDecorationLine: isEnabled ? 'line-through' : 'none',
+           }
+          ]}>
+          {title}
+        </Text>
+      </Pressable>
+    </View>
+    )
+  };
+
+  return (
+    <View>
+      <FlatList
+        style={styles.mainList}
+        data={data} 
+        keyExtractor={item => item.id}
+        renderItem = {
+          ({item}) => <Item title={item.title} id={item.id} />
+          }
+      />
+    <View style={{
+      flexDirection: 'row',
+      alignItems: 'center'
+    }}>
+      <TextInput
+        style={styles.textInput}
+        value={inputText}
+        onChangeText={setInputText}
+        placeholder="Todo..."
+      />
+      <Button
+        id="myButton"
+        onPress={() => handleSubmit()}
+        title="Add Item"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+        disabled={inputText.trim() === ''}
+      />
+      </View>
+    </View>
+  )
+}
+
+export default function App() {
+
+  return (
+      <SafeAreaView style={styles.container}>
+        <MyList data={DATA} />
+      </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainList: {
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'left',
+    padding: 10,
+  },
+  title: {
+	  fontSize: 24
+  },
+  rowItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    maxHeight: 60,
+  },
+
+textInput: {
+  borderColor: 'gray',
+  borderWidth: 1,
+  borderRadius: 8,
+  fontSize: 18,
+  width: 270,
+  paddingLeft: 5,
+  paddingTop: 5,
+  paddingBottom: 5,
+  justifyContent: 'center',
+},
+});
